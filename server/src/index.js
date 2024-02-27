e9df428ed0b57b08c01d5fa92dc7cf5479e7dc01
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-27 19:43:42 +0530

    complete server setup

---
+++ server/src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+dotenv.config();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to BI Test Task");
+});
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
