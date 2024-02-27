53d2909b8019d7a40598d4685573c022449169ea
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-27 19:55:59 +0530

    feat: import pages using lazy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import SuspenseFallback from "./components/fallback";
+import Navbar from "./components/navbar";
+
+const Home = lazy(() => import("./pages/home"));
+const Chat = lazy(() => import("./pages/chat"));
+const Profile = lazy(() => import("./pages/profile"));
+const Products = lazy(() => import("./pages/products"));
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <Navbar />
+
+      <Suspense fallback={<SuspenseFallback />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Suspense>
+    </main>
   );
 }
 
