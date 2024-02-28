16975c4ca73c2dccebf6df716536db485e3f93ed
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-28 16:09:22 +0530

    feat: add context and socket provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,20 @@
 import "./index.css";
 import App from "./App.jsx";
 import React from "react";
+import store from "./app/store.js";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { SocketProvider } from "./context/socketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
