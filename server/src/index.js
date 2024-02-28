1581211360fcc20e4f97e8959fe514743a95bdf8
KashifPathan <kashifpathan.kp.kp@gmail.com>
2024-02-28 17:33:40 +0530

    feat: add socket server

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,10 @@
+import http from "http";
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import { Server } from "socket.io";
 import connectDB from "./db/index.js";
+import { getConversation } from "./socket/index.js";
+
 dotenv.config();
 
 connectDB()
@@ -12,6 +16,26 @@ connectDB()
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
   });
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  //send message
+  socket.on("sendMessage", (data) => {
+    const conversation = getConversation();
+
+    socket.emit("conversation updated", conversation);
+    socket.to(receiverId).emit("conversation updated", conversation);
+  });
+});
 
 // Routes
 app.get("/", (req, res) => {
